fc350639ed645d713a647b5887d8916136c2b373
Elvin Ahmadov <eahmadov2019@ada.edu.az>
2022-01-08 02:04:28 +0400

    error boundary

--- src/components/Selection.js
+++ src/components/Selection.js
@@ -1,14 +1,14 @@
 
-import {Form} from 'react-bootstrap'
+import { Form } from 'react-bootstrap'
 
 const Selection = ({ list = [], defaultValue, onChange, ...rest }) => {
     return (
         <Form.Select aria-label="Default select example" defaultValue={defaultValue} onChange={onChange}{...rest}>
             {list.map(item => {
-                return <option key ={item.value}  value={item.value} > {item.title }</option>
+                return <option key={item.value} value={item.value} > {item.title}</option>
             })}
 
-</Form.Select>
+        </Form.Select>
     )
 }
 export default Selection
